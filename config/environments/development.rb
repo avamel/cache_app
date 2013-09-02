dffff562853c9c805bc0618e71f19b1a9bdb12f6
avamel <xnuamx@gmail.com>
2013-09-02 18:20:38 +0300

    modify memory_store

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,6 @@ CacheApp::Application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-  #config.cache_store = :mem_cache_store, '127.0.0.1:17898'
   config.cache_store = :memory_store
 
   # Don't care if the mailer can't send.
